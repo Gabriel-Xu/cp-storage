269d09fcf04ed181301df8ad1a55fbb6e9937ae7
Gabriel Xu <105399165+Gabriel-Xu@users.noreply.github.com>
2023-12-02 12:03:55 -0500

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+#define forn(z) for(int i=0; i<z; i++)
+#define forn2(z) for(int j=0; j<z; j++)
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL)
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<ll> vll;
+typedef pair<ll,ll> pll;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define fi first
+#define se second
+#define dbg(z, n) forn(n)cerr<<z[i]<<" \n"[i==n-1];
+#define dbg2(z, n, m) forn(n)forn2(m)cerr<<z[i][j]<<" \n"[j==m-1];
+ll int_sqrt(ll x) {ll ans = 0;for (ll k = 1LL << 30; k != 0; k /= 2) {if ((ans + k) * (ans + k) <= x) {ans += k;}}return ans;}
+ll int_pow(ll a, ll b) {ll ans=1;while(b--)ans*=a;return ans;}
+void __print(int x) {cerr << x;}
+void __print(ll x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+int main() {
+	fastIO;
+	int tc;
+	cin >> tc;
+	while(tc--) {
+		
+	}
+}
