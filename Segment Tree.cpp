6879a6f2287b3d6bd27267c734dae6dff3a7cd68
Gabriel Xu <105399165+Gabriel-Xu@users.noreply.github.com>
2024-07-18 16:25:00 -0400

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -17,7 +17,7 @@ template <class T> class SegmentTree {
 			segtree[ind >> 1] = comb(segtree[ind], segtree[ind ^ 1]);
 		}
 	}
-	T range_sum(int start, int end) { // [start, end)
+	T range_query(int start, int end) { // [start, end)
 		assert(0 <= start && start < len && 0 < end && end <= len);
 		T sum = DEFAULT;
 		for (start += len, end += len; start < end; start /= 2, end /= 2) {
@@ -50,7 +50,7 @@ int main() {
 			int start;
 			int end;
 			cin >> start >> end;
-			cout << segtree.range_sum(start - 1, end) << '\n';
+			cout << segtree.range_query(start - 1, end) << '\n';
 		}
 	}
 }
