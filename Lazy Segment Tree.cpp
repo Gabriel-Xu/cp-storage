1e1a9b9099a137ab41adc02de78cc97144480e53
Gabriel Xu <105399165+Gabriel-Xu@users.noreply.github.com>
2024-07-18 18:09:00 -0400

    Update Lazy Segment Tree.cpp

--- Lazy Segment Tree.cpp
+++ Lazy Segment Tree.cpp
@@ -2,79 +2,114 @@
 using namespace std;
 
 template<class T> struct LazySegmentTree {
-    int len;
-    vector<T> tree, lazy;
-    const T TREE_DEFAULT = 0;
-    const T LAZY_DEFAULT = 0;
-    // for queries
-    T tree_merge(T a, T b) {
-        return a+b;
+    vector<T> st;
+    vector<T> lazy;
+    int n;
+    T identity_element;
+    T identity_update;
+    LazySegmentTree(int n, T identity_element, T identity_update) {
+        this->n = n;
+        this->identity_element = identity_element;
+        this->identity_update = identity_update;
+        st.assign(4*n, identity_element);
+        lazy.assign(4*n, identity_update);
     }
-    // for updates
-    T lazy_merge(T a, T b) {
-        return a+b;
+    T combine(T l, T r) { // adjust for query type
+        return l+r;
+        // return max(l, r);
+        // return min(l, r);
     }
-    void lazy_merged(T &a, T b) { a = lazy_merge(a, b); }
-    LazySegmentTree(int n) {
-        len = 1; while (len < n) len *= 2;
-        tree.resize(2 * len, TREE_DEFAULT);
-        lazy.resize(2 * len, LAZY_DEFAULT);
+    T apply(T curr, T upd, int tl, int tr) { // adjust for update/query type
+        T ans = curr + (tr-tl+1)*upd; // range increment, range sum
+        // T ans = curr + upd; // range increment, range max/min
+        // T ans = (tr-tl+1)*upd; // range set, range sum
+        // T ans = upd; // range set, range max/min
+        return ans;
     }
-    void propagate(int x, int lx, int rx) {
-        if (rx - lx == 1) return;
-        lazy_merged(lazy[2 * x + 1], lazy[x]);
-        lazy_merged(lazy[2 * x + 2], lazy[x]);
-        lazy_merged(tree[2 * x + 1], lazy[x]);
-        lazy_merged(tree[2 * x + 2], lazy[x]);
-        lazy[x] = LAZY_DEFAULT;
+    T combineUpdate(T old_upd, T new_upd, int tl, int tr) { // adjust for update
+        T ans = old_upd;
+        ans+=new_upd; // range increment
+        // ans=new_upd; // range set
+        return ans;
     }
-    void add(int l, int r, T v, int x, int lx, int rx) {
-        propagate(x, lx, rx);
-        if (lx >= r || rx <= l) return;
-        if (lx >= l && rx <= r) {
-            lazy_merged(lazy[x], v);
-            lazy_merged(tree[x], v);
+    void buildUtil(int v, int tl, int tr, vector<T>&a) {
+        if(tl == tr) {
+            st[v] = a[tl];
             return;
         }
-        int m = (lx + rx) / 2;
-        add(l, r, v, 2 * x + 1, lx, m);
-        add(l, r, v, 2 * x + 2, m, rx);
-        tree[x] = tree_merge(tree[2 * x + 1], tree[2 * x + 2]);
+        int tm = (tl + tr)>>1;
+        buildUtil(2*v + 1, tl, tm,a);
+        buildUtil(2*v + 2,tm+1,tr,a);
+        st[v] = combine(st[2*v + 1], st[2*v + 2]);
     }
-    T query(int l, int r, int x, int lx, int rx) {
-        propagate(x, lx, rx);
-        if (lx >= r || rx <= l) return TREE_DEFAULT;
-        if (lx >= l && rx <= r) return tree[x];
-        int m = (lx + rx) / 2;
-        T left = query(l, r, 2 * x + 1, lx, m);
-        T right = query(l, r, 2 * x + 2, m, rx);
-        return tree_merge(left, right);
+    void push_down(int v, int tl, int tr) {
+        if(lazy[v] == identity_update)return;
+        st[v] = apply(st[v], lazy[v], tl, tr);
+        if(2*v + 2 < 4*n) {
+            int tm = (tl + tr)>>1;
+            lazy[2*v + 1] = combineUpdate(lazy[2*v+1], lazy[v], tl, tm);
+            lazy[2*v + 2] = combineUpdate(lazy[2*v+2], lazy[v], tm+1,tr);            
+        }
+        lazy[v] = identity_update;
     }
-    void update(int l, int r, T v) { add(l, r, v, 0, 0, len); }
-    T query(int l, int r) { return query(l, r, 0, 0, len); }
+    T queryUtil(int v, int tl, int tr, int l, int r) {
+        push_down(v,tl,tr);
+        if(l > r)return identity_element;
+        if(tr < l or tl > r) {
+            return identity_element;
+        }
+        if(l <= tl and r >= tr) {
+            return st[v];
+        }
+        int tm = (tl + tr)>>1;
+        return combine(queryUtil(2*v+1,tl,tm,l,r), queryUtil(2*v+2,tm+1,tr,l,r));
+    }
+    void updateUtil(int v, int tl, int tr, int l, int r, T upd) {
+        push_down(v,tl,tr); 
+        if(tr < l or tl > r)return;
+        if(tl >=l and tr <=r) {
+            lazy[v] = combineUpdate(lazy[v],upd,tl,tr);
+            push_down(v,tl,tr);
+        }
+        else {
+            int tm = (tl + tr)>>1;
+            updateUtil(2*v+1,tl,tm,l,r,upd);
+            updateUtil(2*v+2,tm+1,tr,l,r,upd);
+            st[v] = combine(st[2*v + 1], st[2*v+2]);
+        }
+    }
+    void build(vector<T>& a) {
+        assert(sz(a) == n);
+        buildUtil(0,0,n-1,a);
+    }
+    T query(int l, int r) {
+        return queryUtil(0,0,n-1,l,r);
+    }
+    void update(int l,int r, T upd) {
+        updateUtil(0,0,n-1,l,r,upd);
+    }
 };
 
 int main() {
-	int n, q;
-	cin >> n >> q;
-    LazySegmentTree<long long> segtree(n);
-    for(int i=0; i<n; i++) {
-        int x; cin >> x;
-        segtree.update(i,i+1,x);
-    }
-    while(q--) {
-        int x; cin >> x;
-        if(x==1) {
-            int a, b, u;
-            cin >> a >> b >> u;
-            a--, b--;
-            segtree.update(a, b+1, u);
-        }
-        else {
-            int k;
-            cin >> k;
-            k--;
-            cout << segtree.query(k, k+1) << "\n";
-        }
-    }
+	int tc; cin >> tc;
+	while(tc--) {
+		int n, q;
+		cin >> n >> q;
+	    LazySegmentTree<long long> segtree(n, 0, 0);
+	    while(q--) {
+	        int x; cin >> x;
+	        if(x==0) {
+	            int a, b, u;
+	            cin >> a >> b >> u;
+                a--, b--;
+	            segtree.update(a, b, u);
+	        }
+	        else {
+	            int a, b;
+	            cin >> a >> b;
+                a--, b--;
+	            cout << segtree.query(a, b) << "\n";
+	        }
+	    }
+	}
 }
